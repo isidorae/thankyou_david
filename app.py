49ae00d6431354f4bbc272b1dc1b5e3d18d39033
isidorae <mariaisidoraerrazuriz@gmail.com>
2023-12-12 19:57:54 -0300

    front end login/register/profile

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #. .venv/bin/activate
 # flask --app app.py --debug run
 from cs50 import SQL
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, jsonify, render_template, session, request, redirect
 from flask_session import Session
 from datetime import date
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -45,7 +46,7 @@ def sendComment():
 
     if name == "" or comment == "" or img =="none":
         print("All fields must be filled")
-        return render_template("comment_form_err.html")
+        return render_template("err/comment_form_err.html")
 
     else:
         print(img, name, comment)
@@ -54,18 +55,117 @@ def sendComment():
 
     # https://api.github.com/users/{username}}
 
+@app.route("/myprofile")
+def myprofile():
+    return render_template("myprofile.html")
 
 @app.route("/error")
 def errormsg():
-    return render_template("comment_form_err.html")
-# @app.route("/comments")
-# def index():
-#     return render_template("index.html")
+    return render_template("err/comment_form_err.html")
 
-# @app.route("/login")
-# def index():
-#     return render_template("index.html")
 
-# @app.route("/register")
-# def index():
-#     return render_template("index.html")
+################################################################ AUTH ################################################################
+
+################################ LOGIN
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+        if not request.form.get("username") or not request.form.get("password"):
+            message="Must enter a valid username & password"
+            err="403"
+            return render_template("err/login_form_error.html", message=message, err=err)
+
+        # find username in db
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists (WE MUST GET ONLY 1 ROW = 1 UNIQUE USERNAME) and password is correct
+        if len(rows) != 1: 
+            message="Username not found in database."
+            err="403"
+            return render_template("err/invalid_login.html")
+        
+        if not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            message="Incorrect password."
+            err="403"
+            return render_template("err/invalid_login.html")
+
+        # Remember which user has logged in -> keep track of info; store current user ID
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+################################ LOGOUT
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+################################ REGISTER
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # POST -> check errors & submit data to table database -> log user in
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        passconfirm = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not username:
+            message = "Must fill out all fields."
+            err= "400"
+            return render_template("err/register_form_err.html", message=message, err=err)
+
+        # Ensure password was submitted
+        elif not password or not passconfirm:
+            message = "Must fill out all fields."
+            err= "400"
+            return render_template("err/register_form_err.html", message=message, err=err)
+
+        # check if username exists
+        elif db.execute("SELECT * FROM users WHERE username = ?", username):
+            message = "Username already taken :("
+            err= "400"
+            return render_template("err/register_form_err.html", message=message, err=err)
+
+        # check password is confirmed
+        elif password != passconfirm:
+            message = "Password must be the same as confirmation."
+            err= "400"
+            return render_template("err/register_form_err.html", message=message, err=err)
+
+        # hash password
+        hash = generate_password_hash(password)
+
+        # insert data in users table
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+
+        # store session
+        user_data = db.execute("SELECT * FROM users WHERE username = ?", username)
+        session["user_id"] = user_data[0]["id"]
+
+        return redirect("/")
+
+    # GET -> display form to register
+    else:
+        return render_template("register.html")
